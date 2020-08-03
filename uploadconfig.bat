ec60ee245c67c0e686f8fc6eb265d3ad7f39d8fa
byzombies <chris.gr.clark@gmail.com>
2020-08-04 11:19:24 +1200

    bat file

---
+++ uploadconfig.bat
@@ -0,0 +1,2 @@
+rmdir -R .\Config\
+xCopy /E %userprofile%\appdata\locallow\"Ludeon Studios"\"RimWorld by Ludeon Studios"\Config .\Config\
--- uploadconfig.sh
+++
@@ -1,5 +0,0 @@
-rmdir -R .\Config\
-xCopy /E %userprofile%\appdata\locallow\"Ludeon Studios"\"RimWorld by Ludeon Studios"\Config .\Config\
-git add *
-git commit -m "Config pulled - %DATE% %TIME%"
-git push
